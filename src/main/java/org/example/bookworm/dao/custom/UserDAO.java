ce415e33a24946d9f2b1b992c79f3d2bc5a8029f
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-13 04:58:52 +0530

    Finished Creating UserDao files

--- src/main/java/org/example/bookworm/dao/custom/UserDAO.java
+++ src/main/java/org/example/bookworm/dao/custom/UserDAO.java
@@ -2,6 +2,8 @@ package org.example.bookworm.dao.custom;
 
 import org.example.bookworm.dao.CrudDAO;
 import org.example.bookworm.entity.User;
+import org.hibernate.Session;
 
 public interface UserDAO extends CrudDAO <User, String> {
+    Integer getUserCount(Session session);
 }
--- src/main/java/org/example/bookworm/dao/custom/impl/UserDaoImpl.java
+++ src/main/java/org/example/bookworm/dao/custom/impl/UserDaoImpl.java
@@ -3,43 +3,60 @@ package org.example.bookworm.dao.custom.impl;
 import org.example.bookworm.dao.custom.UserDAO;
 import org.example.bookworm.entity.User;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserDaoImpl implements UserDAO {
     @Override
     public ArrayList<User> getAll(Session session) throws SQLException, ClassNotFoundException {
-        return null;
+        Query query = session.createQuery("FROM User", User.class);
+        List<User> list = query.list();
+        return (ArrayList<User>) list;
     }
 
     @Override
-    public boolean save(User data, Session session) throws SQLException, ClassNotFoundException {
+    public boolean save(User entity, Session session) throws SQLException, ClassNotFoundException {
         session.save(entity);
+        return true;
     }
 
     @Override
-    public boolean update(User data, Session session) throws SQLException, ClassNotFoundException {
-        return false;
-    }
-
-    @Override
-    public boolean exits(String s) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean update(User entity, Session session) throws SQLException, ClassNotFoundException {
+        session.update(entity);
+        return true;
     }
 
     @Override
     public boolean delete(String s, Session session) throws SQLException, ClassNotFoundException {
-        return false;
+        User user = new User();
+        user.setUserID(s);
+        session.delete(user);
+        return true;
     }
 
     @Override
     public String generateNewId(Session session) throws SQLException, ClassNotFoundException {
-        return null;
+        Query query = session.createQuery("SELECT userID FROM User ORDER BY userID DESC");
+        query.setMaxResults(1);
+        List list = query.list();
+        return (list.size() == 0) ? null : (String) list.get(0);
     }
 
     @Override
-    public User search(String Value, Session session) throws SQLException, ClassNotFoundException {
-        return null;
+    public User search(String id, Session session) throws SQLException, ClassNotFoundException {
+        try(session){
+            return session.get(User.class, id);
+        }
+    }
+
+    @Override
+    public Integer getUserCount(Session session) {
+        String hql = "SELECT COUNT(userID) FROM User";
+        Query<Long> query = session.createQuery(hql, Long.class);
+        Long count = query.uniqueResult();
+        return Math.toIntExact(count);
     }
 }
