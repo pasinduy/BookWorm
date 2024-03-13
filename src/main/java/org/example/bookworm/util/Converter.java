33c64fb99623f85a41ac5611d495756416830923
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-13 22:02:11 +0530

    Converter Done

--- src/main/java/org/example/bookworm/util/Converter.java
+++ src/main/java/org/example/bookworm/util/Converter.java
@@ -1,7 +1,7 @@
 package org.example.bookworm.util;
 
-import org.example.bookworm.dto.BookDto;
-import org.example.bookworm.entity.Book;
+import org.example.bookworm.dto.*;
+import org.example.bookworm.entity.*;
 
 public class Converter {
     private static Converter converter;
@@ -10,6 +10,11 @@ public class Converter {
 
     public static Converter getInstance(){return converter == null? converter = new Converter() : converter;}
 
+    public Admin toAdminEntity (AdminDto dto){
+        Admin admin = new Admin();
+
+        return admin;
+    }
     public Book toBookEntity(BookDto dto){
         Book book = new Book();
         book.setTitle(book.getTitle());
@@ -19,12 +24,58 @@ public class Converter {
         return book;
     }
 
+    public Borrow toBorrowEntity(BorrowDto dto){
+        Borrow borrow = new Borrow();
+
+        return borrow;
+    }
+
+    public Branch toBranchEntity(BranchDto dto){
+        Branch branch = new Branch();
+
+        return branch;
+    }
+
+    public User toUserEntity(UserDto dto){
+        User user = new User();
+        user.setUserID(user.getUserID());
+        user.setEmail(user.getEmail());
+        user.setPassword(user.getPassword());
+        return user;
+    }
+
+    public AdminDto toAdminDto(Admin admin){
+        AdminDto dto = new AdminDto();
+
+        return dto;
+    }
+
+    public BorrowDto toBorrowDto(Borrow borrow){
+        BorrowDto dto = new BorrowDto();
+
+        return dto;
+    }
+
     public BookDto toBookDto(Book book){
         BookDto dto = new BookDto();
         dto.setTitle(dto.getTitle());
         dto.setAuthor(dto.getAuthor());
         dto.setGenre(dto.getGenre());
         dto.setStatus(dto.getStatus());
+        return dto;
+    }
+
+    public BranchDto toBranchDto (Branch branch){
+        BranchDto dto = new BranchDto();
+
+        return dto;
+    }
+
+    public UserDto toUserDto (User user) {
+        UserDto dto = new UserDto();
+        dto.setUserID(dto.getUserID());
+        dto.setEmail(dto.getEmail());
+        dto.setPassword(dto.getPassword());
         return dto;
     }
 
