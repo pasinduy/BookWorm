12a9a16ce8f7df6274fd552149b6f82c44bd42d8
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-14 00:20:32 +0530

    Branch Files

--- src/main/java/org/example/bookworm/dto/BranchDto.java
+++ src/main/java/org/example/bookworm/dto/BranchDto.java
@@ -1,4 +1,14 @@
 package org.example.bookworm.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BranchDto {
+    private String branchId;
+    private String branchName;
+    private String contactNo;
 }
