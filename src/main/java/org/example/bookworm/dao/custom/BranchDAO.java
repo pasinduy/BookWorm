869336b095155ef75c694509cd0475ab1155fb44
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-15 22:02:13 +0530

    Branch Files

--- src/main/java/org/example/bookworm/dao/custom/BranchDAO.java
+++
@@ -1,7 +0,0 @@
-package org.example.bookworm.dao.custom;
-
-import org.example.bookworm.dao.CrudDAO;
-import org.example.bookworm.entity.Branch;
-
-public interface BranchDAO extends CrudDAO<Branch, String> {
-}
