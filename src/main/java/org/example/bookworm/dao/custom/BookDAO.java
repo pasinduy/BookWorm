c223747721e26945bdb41c5e17d8520f57d479ea
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-15 22:01:32 +0530

    Book Files

--- src/main/java/org/example/bookworm/dao/custom/BookDAO.java
+++
@@ -1,7 +0,0 @@
-package org.example.bookworm.dao.custom;
-
-import org.example.bookworm.dao.CrudDAO;
-import org.example.bookworm.entity.Book;
-
-public interface BookDAO extends CrudDAO<Book, String> {
-}
