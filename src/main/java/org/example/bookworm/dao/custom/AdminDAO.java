cda79b852cb7534bac3783c5a2141566f40b3055
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-15 21:58:11 +0530

    Fixed Admin Files

--- src/main/java/org/example/bookworm/dao/custom/AdminDAO.java
+++
@@ -1,7 +0,0 @@
-package org.example.bookworm.dao.custom;
-
-import org.example.bookworm.dao.CrudDAO;
-import org.example.bookworm.entity.Admin;
-
-public interface AdminDAO extends CrudDAO<Admin,String> {
-}
