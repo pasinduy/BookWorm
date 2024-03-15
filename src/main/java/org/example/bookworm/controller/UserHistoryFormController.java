b0183b653d29e4ae8abc1c361afae745b3d53d55
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-15 22:03:23 +0530

    User Files

--- src/main/java/org/example/bookworm/controller/UserHistoryFormController.java
+++
@@ -1,9 +0,0 @@
-package org.example.bookworm.controller;
-
-import javafx.scene.control.TableColumn;
-
-public class UserHistoryFormController {
-    public TableColumn clmTitle;
-    public TableColumn clmDate;
-    public TableColumn clmStatus;
-}
