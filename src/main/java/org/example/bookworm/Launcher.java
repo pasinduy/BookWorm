067e77d22db8c5527dd4d2d59f5faff28ee4726d
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-03-18 10:33:44 +0530

    All Done !

--- src/main/java/org/example/bookworm/Launcher.java
+++ src/main/java/org/example/bookworm/Launcher.java
@@ -8,16 +8,14 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.IOException;
-
-public class Main extends Application {
+public class Launcher extends Application {
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("org/example/bookworm/view/loading.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loading.fxml"));
         Scene scene = new Scene(root, 500,500, Color.TRANSPARENT);
         root.setStyle("-fx-background-color: transparent;");
         stage.initStyle(StageStyle.TRANSPARENT);
---
+++ src/main/java/org/example/bookworm/LauncherWrapper.java
@@ -0,0 +1,7 @@
+package org.example.bookworm;
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
